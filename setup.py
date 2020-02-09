8702988667ede2b0da9bab3b74c6079ab33448f5
Wim Muskee <wimmuskee@gmail.com>
2020-02-09 17:31:41 +0100

    add importable version

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 import os
 from setuptools import setup, find_packages
+import gengee
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "gengee",
-    version = "2.0.0",
+    version = gengee.__version__,
     author = "Wim Muskee",
     author_email = "wimmuskee@gmail.com",
     url = "https://github.com/wimmuskee/gengee",
